f323495a03f76454119c60f7949d4faeab9eb9dd
Siva166 <30434368+Siva166@users.noreply.github.com>
2017-08-10 19:26:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -87,8 +87,8 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/:articleNames', function (req, res) {
-    var articleName = req.params.articleNames;
+app.get('/:articleName', function (req, res) {
+  var articleName = req.params.articleName;
   res.send(createTemplate(articles[articleName]));
 });
 
