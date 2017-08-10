cb022eab3c878d37f9f0fc6ca5242b8fb495d286
Siva166 <30434368+Siva166@users.noreply.github.com>
2017-08-10 18:19:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,68 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleOne = {
+    title: "Article One - Siva",
+    heading: "Article One",
+    date: "Aug 10, 2017"
+    content: `
+            <p>
+                This is the sample content for first article. This is the sample content for first article. This is the sample content for first article.
+                This is the sample content for first article. This is the sample content for first article. This is the sample content for first article.
+                This is the sample content for first article. This is the sample content for first article. This is the sample content for first article.
+            </p>
+            <p>
+                This is the sample content for first article. This is the sample content for first article. This is the sample content for first article.
+                This is the sample content for first article. This is the sample content for first article. This is the sample content for first article.
+                This is the sample content for first article. This is the sample content for first article. This is the sample content for first article.
+            </p>
+            <p>
+                This is the sample content for first article. This is the sample content for first article. This is the sample content for first article.
+                This is the sample content for first article. This is the sample content for first article. This is the sample content for first article.
+                This is the sample content for first article. This is the sample content for first article. This is the sample content for first article.
+            </p>`
+}
+
+function createTemplate(data){
+var title = data.title;
+var heading = data.heading;
+var date = data.date;
+var content = data.content;
+var htmlTemplate = `
+    <html>
+        <head>
+            <title>
+                ${title}
+            </title>
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <link href="/ui/style.css" rel="stylesheet" />
+        </head>
+        <body>
+            <div class="container">
+                <div>
+                    <a href="/">Home</a>
+                </div>
+                <hr/>
+                <h3>${heading}</h3>
+                <div>
+                    ${date}
+                </div>
+                <div>
+                ${content}
+                </div>
+            </div>
+        </body>
+    </html>
+    `;
+return htmlTemplate;
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one.html', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+  res.send(createTemplate(articleOne)));
 });
 
 app.get('/article-two.html', function (req, res) {
