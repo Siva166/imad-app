65a9d9a92d7a2792607f558302bda4cb9dbe003e
Siva166 <30434368+Siva166@users.noreply.github.com>
2017-08-14 17:36:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,6 @@
 console.log('Loaded!');
 var element = document.getElementById("main-text");
-element.innerHTML = "New change";
+//element.innerHTML = "New change";
 var img = document.getElementById("madi");
 var mRight = 1;
 function moveRight() {
