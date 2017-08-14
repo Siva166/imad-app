97ecf396cffa728ecc606f847b7c715180bee2b6
Siva166 <30434368+Siva166@users.noreply.github.com>
2017-08-14 15:49:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,10 +4,10 @@ element.innerHTML = "New change";
 var img = document.getElementById("madi");
 var mRight = 1;
 function moveRight() {
-    mRight = mRight + 10;
+    mRight = mRight + 11;
     img.style.marginLeft = mRight + 'px';
 }
 img.onclick = function(){
-    var interval = setInterval(moveRight, 100);
+    var interval = setInterval(moveRight, 50);
     //img.style.marginLeft = mLeft + 'px';
 };
