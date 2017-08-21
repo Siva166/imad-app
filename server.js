13d1684ecc146ed693114df8515ecb2e8adbfcc5
Siva166 <30434368+Siva166@users.noreply.github.com>
2017-08-21 20:41:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -80,7 +80,7 @@ var htmlTemplate = `
                 <hr/>
                 <h3>${heading}</h3>
                 <div>
-                    ${date}
+                    ${date.toDateString()}
                 </div>
                 <div>
                 ${content}
