37b475f98ac2829a895c0c3715c4a102ce3f5edd
Siva166 <30434368+Siva166@users.noreply.github.com>
2017-08-21 19:53:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+
+var config = {
+    user: 'shivaothers',
+    database: 'shivaothers',
+    host: 'db.imad.hasura-app.io',
+    port: '5432',
+    password: process.env.DB_PASSWORD
+};
 
 var app = express();
 app.use(morgan('combined'));
@@ -85,6 +94,20 @@ return htmlTemplate;
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+var pool = new Pool(config);
+app.get('/test-db', function (req, res) {
+    // make a select request
+    // return a response with the results
+  pool.query('select * from test', function(err, result){
+     if (err) {
+         res.status(500).send(err.tostring());
+     } else {
+         res.send(JSON.stringify(result));
+     }
+     
+  });
 });
 
 var counter = 0;
