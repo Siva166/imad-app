af88da65eade51f1aa7fc150db23a45e17884e43
Siva166 <30434368+Siva166@users.noreply.github.com>
2017-08-23 15:45:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
+var crypto = require('crypto');
 
 var config = {
     user: 'shivaothers',
@@ -94,6 +95,17 @@ return htmlTemplate;
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+function hash(input, salt){
+    var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512,'sha512');
+    //return hashed;
+    return hashed.toString('hex');
+}
+
+app.get('/hash/:input', function(req, res){
+   var hashString = hash(req.params.input, 'this-is-some-random-string');
+   res.send(hashString);
 });
 
 var pool = new Pool(config);
