c92a1ad91840de28005090f34692b0d576b24887
Siva166 <30434368+Siva166@users.noreply.github.com>
2017-08-23 19:44:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,13 +1,29 @@
-console.log('Loaded!');
-var element = document.getElementById("main-text");
-element.innerHTML = "New change";
-var img = document.getElementById("madi");
-var mRight = 1;
-function moveRight() {
-    mRight = mRight + 11;
-    img.style.marginLeft = mRight + 'px';
-}
-img.onclick = function(){
-    var interval = setInterval(moveRight, 50);
-    //img.style.marginLeft = mLeft + 'px';
+
+// submit username/password to login
+var submit = document.getElementById('submit_btn');
+submit.onclick = function(){
+  // create a request object
+  var request = new XMLHttpRequest();
+  request.onreadystatechange = function(){
+    if (request.readyState === XMLHttpRequest.DONE) {
+        if (request.status === 200) {
+            console.log('user logged in');
+            alert('Logged in successfully');
+        } else if (request.status === 403){
+            alert('Username/password is incorrect');
+        } else if (request.status === 403){
+            alert('Something went wrong on the server');
+        }    
+    }
+    // Not done yet
+  };
+
+    // Make the request
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    console.log(username);
+    console.log(password);
+    request.open('POST', 'http://shivaothers.imad.hasura-app.io/login', true);
+    request.setRequestHeader('Content-Type', 'application/json');
+    request.send(JSON.stringify({username:username, password: password}));
 };
